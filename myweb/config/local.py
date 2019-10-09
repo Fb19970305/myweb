05c83127f2cf41ee0f84eef8cc6d97541bc9a089
Fb19970305 <menghuitiaoling@outlook.com>
2019-10-09 09:29:56 +0800

    第一版

---
+++ myweb/config/local.py
@@ -0,0 +1,18 @@
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myweb',
+        'USER': 'root',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
+    },
+
+}
---
+++ myweb/config/product.py
@@ -0,0 +1,14 @@
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Fb19970305$myweb',
+        'USER': 'Fb19970305',
+        'PASSWORD': 'Vivo12##',
+        'HOST': 'Fb19970305.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+    },
+
+}
