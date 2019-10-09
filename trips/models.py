3cd725c5837fc4c403fcfe687c0062ddfa86ff3e
Fb19970305 <menghuitiaoling@outlook.com>
2019-10-09 09:53:51 +0800

    修改model

--- trips/models.py
+++ trips/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     content = models.TextField(blank=True)
     photo = models.URLField(blank=True)
     location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
